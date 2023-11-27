33d49f3b26a2d3779ae3cecff8fda6bed0bcde1a
Karlo Peterfaj <142494540+KarloPet@users.noreply.github.com>
2023-11-27 17:25:44 +0100

    Update booking_system_DB.sql

--- booking_system_DB.sql
+++ booking_system_DB.sql
@@ -1,38 +1,39 @@
---drop edunovawp2;
+drop edunovawp2;
 
-----use master;
+--use master;
 
---create database booking_system;
+create database booking_system;
 
---use booking_system;
+use booking_system;
 
---drop table gost;
+drop table gost;
 
---create table gost (
---ime varchar (50)not null,
---prezime varchar (50)not null,
---oib int not null primary key identity(1,1),
---grupa int not null
---);
+create table gost (
+id int not null primary key identity(1,1),
+ime varchar (50)not null,
+prezime varchar (50)not null,
+oib varchar (11)not null,
+grupa int not null
+);
 
---create table grupa (
---id int not null primary key identity(1,1),
---rezervacija int not null
---);
+create table grupa (
+id int not null primary key identity(1,1),
+rezervacija int not null
+);
 
---create table rezervacija (
---id int not null primary key identity(1,1),
---cijena decimal (18,2) not null,
---datum_od datetime not null,
---datum_do datetime not null
---);
+create table rezervacija (
+id int not null primary key identity(1,1),
+cijena decimal (18,2) not null,
+datum_od datetime not null,
+datum_do datetime not null
+);
 
---create table cjenik (
---id int not null primary key identity(1,1),
---datum datetime not null,
---cijena decimal (18,2) not null,
---rezervirano bit not null
---);
+create table cjenik (
+id int not null primary key identity(1,1),
+datum datetime not null,
+cijena decimal (18,2) not null,
+rezervirano bit not null
+);
 
---alter table gost add foreign key (grupa) references grupa(id);
---alter table grupa add foreign key (rezervacija) references rezervacija(id);
+alter table gost add foreign key (id) references grupa(id);
+alter table grupa add foreign key (rezervacija) references rezervacija(id);
