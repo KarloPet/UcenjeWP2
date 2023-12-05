7a24d924cc739a16d11634ba572d00e9b6a7e691
Karlo Peterfaj <142494540+KarloPet@users.noreply.github.com>
2023-12-05 17:42:53 +0100

    Update booking_system_DB.sql

--- booking_system_DB.sql
+++ booking_system_DB.sql
@@ -1,36 +1,36 @@
 ﻿use master;
 
-drop database	 booking_system;
+drop database booking_system;
 
-create database	 booking_system;
+create database booking_system;
 
-use				 booking_system;
+use booking_system;
 
 create table gost (
-	id		 int			not null primary key identity(1,1),
-	ime		 varchar(50)	not null,
-	prezime	 varchar(50)	not null,
-	oib		 varchar(11)	not null,
-	grupa	 int			not null
+	id int not null primary key identity(1,1),
+	ime varchar (50)not null,
+	prezime varchar (50)not null,
+	oib varchar (11)not null,
+	grupa int not null
 );
 
 create table grupa (
-	id			int			not null primary key identity(1,1),
-	rezervacija int			not null
+	id int not null primary key identity(1,1),
+	rezervacija int not null
 );
 
 create table rezervacija (
-	id			int				not null primary key identity(1,1),
-	cijena	    decimal(18,2)	not null,
-	datum_od	datetime		not null,
-	datum_do	datetime		not null
+	id int not null primary key identity(1,1),
+	cijena decimal (18,2) not null,
+	datum_od datetime not null,
+	datum_do datetime not null
 );
 
 create table cjenik (
-	id			int				not null primary key identity(1,1),
-	datum		datetime		not null,
-	cijena		decimal(18,2)	not null,
-	rezervirano bit				not null
+	id int not null primary key identity(1,1),
+	datum datetime not null,
+	cijena decimal (18,2) not null,
+	rezervirano bit not null
 );
 
 alter table gost add foreign key (grupa) references grupa(id);
